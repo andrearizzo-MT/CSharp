e2c28d334e952c234f9f3c6547e82946ee2d1102
andrearizzo-MT <andrearizzo85@gmail.com>
2017-08-06 12:30:57 +0200

    Added WriteLine to output on screen.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. This is my first C# Console Program! :)");
+        }
+    }
+}
